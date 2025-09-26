0e9902a6921868522e13049997c317dce9abe6ac
Zhixin Li <lzhx04@gmail.com>
2025-09-26 04:08:20 -0400

    Finalized alu & testbench

--- lab1/lab1.srcs/sources_1/new/alu.sv
+++ lab1/lab1.srcs/sources_1/new/alu.sv
@@ -20,24 +20,80 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module alu(
-    input wire op1,
-    input wire op2,
-    input wire [2:0] control,
-    input wire error,
-    output reg res
-    );
+module alu #(
+    parameter OP_WIDTH = 1
+) (
+    input wire [OP_WIDTH-1:0] op1,
+    input wire [OP_WIDTH-1:0] op2,
+    input wire [3:0] control,
+    output reg error,
+    output reg [OP_WIDTH-1:0] res
+);
     always_comb begin
-        if (control == 0) begin // NOT
+        assign error = 1'b0;
+        if (control == 4'b0000) begin // NOT
             res = ~op1;
-        end else if (control == 1) begin // XOR
+        end else if (control == 4'b0001) begin // XOR
             res = op1 ^ op2;
-        end else if (control == 10) begin // AND
+        end else if (control == 4'b0010) begin // AND
             res = op1 & op2;
-        end else if (control == 11) begin // OR
+        end else if (control == 4'b0011) begin // OR
             res = op1 | op2;
-        end else begin // XNOR
+        end else if (control == 4'b0100) begin // XNOR
             res = ~(op1 ^ op2);
+        end else if (control == 4'b0101) begin // LOG LSHIFT
+            if (op2 > OP_WIDTH) begin
+                res = -1;
+                assign error = 1'b1;
+            end else begin
+                res = op1 << op2;
+            end
+        end else if (control == 4'b0110) begin // LOG RSHIFT
+            if (op2 > OP_WIDTH) begin
+                res = -1;
+                assign error = 1'b1;
+            end else begin
+                res = op1 >> op2;
+            end
+        end else if (control == 4'b0111) begin // ART LSHIFT
+            if (op2 > OP_WIDTH) begin
+                res = -1;
+                assign error = 1'b1;
+            end else begin
+                res = op1 <<< op2;
+            end
+        end else if (control == 4'b1000) begin // ART RSHIFT
+            if (op2 > OP_WIDTH) begin
+                res = -1;
+                assign error = 1'b1;
+            end else begin
+                res = op1 >>> op2;
+            end
+        end else if (control == 4'b1001) begin // ADD
+            res = op1 + op2;
+        end else if (control == 4'b1010) begin // SUB
+            res = op1 - op2;
+        end else if (control == 4'b1011) begin // EQ
+            if (op1 == op2) begin
+                res = 1;
+            end else begin
+                res = 0;
+            end
+        end else if (control == 4'b1100) begin // LT
+            if (op1 < op2) begin
+                res = 1;
+            end else begin
+                res = 0;
+            end
+        end else if (control == 4'b1101) begin // GT
+            if (op1 > op2) begin
+                res = 1;
+            end else begin
+                res = 0;
+            end
+        end else begin
+            res = -1;
+            assign error =  1'b1;
         end
     end
 endmodule
